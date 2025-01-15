50f0aa72a7b08d51a44afebb60d9d92c189024f8
pedromchd <pedrogmachado28@outlook.com>
2025-01-15 13:51:55 -0300

    feat: refactor Estoque class to use private attributes for better encapsulation

--- lib/estoque.py
+++ lib/estoque.py
@@ -3,46 +3,46 @@ import csv
 
 class Estoque:
     def __init__(self, csv_file: str):
-        self.csv_file = csv_file
-        self.fields = []
-        self.produtos = dict()
-        self.carregar_produtos()
+        self._csv_file = csv_file
+        self._fields = []
+        self._produtos = dict()
+        self._carregar_produtos()
 
-    def carregar_produtos(self):
-        with open(self.csv_file, "r", encoding="utf-8", newline="") as file:
+    def _carregar_produtos(self):
+        with open(self._csv_file, "r", encoding="utf-8", newline="") as file:
             reader = csv.DictReader(file)
-            self.fields = reader.fieldnames
-            self.produtos = {row["ID"]: row for row in reader}
+            self._fields = reader.fieldnames
+            self._produtos = {row["ID"]: row for row in reader}
 
-    def salvar_produtos(self):
-        with open(self.csv_file, "w", encoding="utf-8", newline="") as file:
-            writer = csv.DictWriter(file, fieldnames=self.fields)
+    def _salvar_produtos(self):
+        with open(self._csv_file, "w", encoding="utf-8", newline="") as file:
+            writer = csv.DictWriter(file, fieldnames=self._fields)
             writer.writeheader()
-            writer.writerows(self.produtos.values())
+            writer.writerows(self._produtos.values())
 
     def adicionar_produto(self, produto: dict):
         id_produto = produto["ID"]
-        if id_produto in self.produtos:
+        if id_produto in self._produtos:
             raise ValueError("Produto já cadastrado")
-        self.produtos[id_produto] = produto
-        self.salvar_produtos()
+        self._produtos[id_produto] = produto
+        self._salvar_produtos()
 
     def remover_produto(self, id_produto: str):
-        if id_produto not in self.produtos:
+        if id_produto not in self._produtos:
             raise ValueError("Produto não encontrado")
-        del self.produtos[id_produto]
-        self.salvar_produtos()
+        del self._produtos[id_produto]
+        self._salvar_produtos()
 
     def atualizar_produto(self, id_produto: str, produto: dict):
-        if id_produto not in self.produtos:
+        if id_produto not in self._produtos:
             raise ValueError("Produto não encontrado")
-        self.produtos[id_produto] = produto
-        self.salvar_produtos()
+        self._produtos[id_produto] = produto
+        self._salvar_produtos()
 
     def listar_produtos(
         self, nome_produto: str = None, pagina: int = 1, tamanho_pagina: int = 10
     ):
-        produtos = list(self.produtos.values())
+        produtos = list(self._produtos.values())
         if nome_produto:
             produtos = [
                 produto
