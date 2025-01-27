8668bab1c8b6eebd637028d27f2443fa8b3224de
pedromchd <pedrogmachado28@outlook.com>
2025-01-26 23:45:42 -0300

    feat: add Produto class for product representation and serialization

--- lib/estoque.py
+++ lib/estoque.py
@@ -1,6 +1,36 @@
 import csv
 
 
+class Produto:
+    def __init__(
+        self, id: str, nome: str, categoria: str, quantidade: int, preco: float
+    ):
+        self.id = id
+        self.nome = nome
+        self.categoria = categoria
+        self.quantidade = quantidade
+        self.preco = preco
+
+    def to_dict(self):
+        return {
+            "ID": self.id,
+            "Nome": self.nome,
+            "Categoria": self.categoria,
+            "Quantidade": self.quantidade,
+            "Preço": self.preco,
+        }
+
+    @staticmethod
+    def from_dict(data: dict):
+        return Produto(
+            data["ID"],
+            data["Nome"],
+            data["Categoria"],
+            data["Quantidade"],
+            data["Preço"],
+        )
+
+
 class Estoque:
     def __init__(self, csv_file: str):
         self._csv_file = csv_file
