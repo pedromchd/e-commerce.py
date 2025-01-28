2723c2c8d5c2ecb4a12ca1ff4585d6d1a456beda
pedromchd <pedrogmachado28@outlook.com>
2025-01-28 18:32:38 -0300

    refactor: optimize product management in Estoque class using dictionary for storage

--- lib/estoque.py
+++ lib/estoque.py
@@ -10,40 +10,41 @@ class Estoque:
     def carregar_produtos(self):
         with open(self.csv_file, "r") as f:
             reader = csv.DictReader(f)
-            return list(reader)
+            return {row["ID"]: row for row in reader}
 
     def salvar_produtos(self):
         with open(self.csv_file, "w") as f:
             writer = csv.DictWriter(f, fieldnames=self.fields)
             writer.writeheader()
-            writer.writerows(self.produtos)
+            writer.writerows(self.produtos.values())
 
     def adicionar_produto(self, produto):
-        self.produtos.append(produto)
+        self.produtos[produto["ID"]] = produto
         self.salvar_produtos()
 
     def remover_produto(self, id_produto):
-        self.produtos = [p for p in self.produtos if p["ID"] != id_produto]
-        self.salvar_produtos()
+        if id_produto in self.produtos:
+            del self.produtos[id_produto]
+            self.salvar_produtos()
 
     def atualizar_produto(self, id_produto, produto):
-        for p in self.produtos:
-            if p["ID"] == id_produto:
-                p.update(produto)
-                break
-        self.salvar_produtos()
+        if id_produto in self.produtos:
+            self.produtos[id_produto].update(produto)
+            self.salvar_produtos()
 
     def buscar_produtos(self, nome_produto="", tamanho_pagina=10, pagina=1):
-        produtos = [
-            p for p in self.produtos if nome_produto.lower() in p["Nome"].lower()
+        produtos_filtrados = [
+            p
+            for p in self.produtos.values()
+            if nome_produto.lower() in p["Nome"].lower()
         ]
         inicio = (pagina - 1) * tamanho_pagina
         fim = inicio + tamanho_pagina
-        return produtos[inicio:fim]
+        return produtos_filtrados[inicio:fim]
 
     def comprar_produto(self, id_produto, quantidade):
-        for p in self.produtos:
-            if p["ID"] == id_produto:
-                p["Quantidade"] = int(p["Quantidade"]) - quantidade
-                break
-        self.salvar_produtos()
+        if id_produto in self.produtos:
+            self.produtos[id_produto]["Quantidade"] = (
+                int(self.produtos[id_produto]["Quantidade"]) - quantidade
+            )
+            self.salvar_produtos()
